f1d2611e207755421c83abf77c35317c9acf2a31
Simon Cheng <simoncheng559@gmail.com>
2021-05-19 16:02:29 -0400

    make sharedModule live up to its name

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import path = require('path');
 import express = require('express');
+import { sharedModule } from './sharedModule';
 
 const port = parseInt(process.env.PORT || '') || 8080;
 
@@ -7,4 +8,4 @@ const app = express();
 // note that this will be lib/public with webpack.min.js already there when this gets compiled and run
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log('Typescript webpack empty template running on port ' + port));
+app.listen(port, () => console.log(`Typescript webpack empty template running on port ${port}.\nOh and one more thing: ${sharedModule()}`));
