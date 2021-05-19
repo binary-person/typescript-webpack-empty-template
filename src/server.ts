896dab26ac27a5d8aa6bb08065012dde0f86dbf0
Simon Cheng <simoncheng559@gmail.com>
2021-05-19 16:16:29 -0400

    run prettier

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,6 @@ const app = express();
 // note that this will be lib/public with webpack.min.js already there when this gets compiled and run
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Typescript webpack empty template running on port ${port}.\nOh and one more thing: ${sharedModule()}`));
+app.listen(port, () =>
+    console.log(`Typescript webpack empty template running on port ${port}.\nOh and one more thing: ${sharedModule()}`)
+);
